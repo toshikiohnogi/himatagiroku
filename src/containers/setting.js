5eb71dbb0513031b174d636c30f81c054666b18b
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2019-09-15 09:15:10 +0900

    update: 日付パラメータリセット

--- src/containers/setting.js
+++ src/containers/setting.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 
 import WebhookPaper from '../components/setting/WebhookPaper';
+import ResetParameterPaper from '../components/setting/ResetParameterPaper';
+
+const useStyles = makeStyles(() => ({
+  paperMarginTop: {
+    marginTop: 20
+  }
+}));
 
 function SettingContainer () {
+  const classes = useStyles();
+
   return (
     <div>
       <WebhookPaper />
+      <div className={classes.paperMarginTop}></div>
+      <ResetParameterPaper />
     </div>
   );
 }
